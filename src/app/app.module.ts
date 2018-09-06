afb3edaeeda01d8e85ab2c129f12bbc198455fbf
Thomas Han <ethomas.han@gmail.com>
2018-09-06 22:28:16 +1200

    Update app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SwipeableCardsComponent } from './swipeable-cards/swipeable-cards.component';
+import { SwipeablePagesComponent } from './swipeable-pages/swipeable-pages.component';
+
+export const appRoutes: Routes = [
+  { path: 'swipe-card', component: SwipeableCardsComponent },
+  { path: 'swipe-page', component: SwipeablePagesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwipeableCardsComponent,
+    SwipeablePagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [
     { provide: 'Flipping', useValue: window['Flipping'] },
